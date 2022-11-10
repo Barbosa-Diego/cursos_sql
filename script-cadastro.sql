10c56d3b27884e8edb3bb21ee73f10c32fc90904
Diego Barbosa <diego.barbosa.pro@gmail.com>
2022-11-10 10:24:05 -0300

    Add files via upload

---
+++ script-cadastro.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE cadastro
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+SHOW DATABASES;
+USE cadastro;
+
+CREATE TABLE pessoas (
+id INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(30) NOT NULL,
+nascimento DATE,
+sexo ENUM('M','F'),
+peso DECIMAL(5,2),
+altura DECIMAL(3,2),
+nacionalidade VARCHAR(20),
+PRIMARY KEY (id)
+)DEFAULT CHARSET = utf8;
