b872ea18e2066b1e869570bcbc855159f1f92855
Diego Barbosa <diego.barbosa.pro@gmail.com>
2022-12-02 10:42:52 -0300

    Update cadastro.sql

--- cadastro.sql
+++ cadastro.sql
@@ -231,13 +231,13 @@ ORDER BY COUNT(*);
 /* Mais Execícios  */
 
 /* 10) Uma lista de com as profissões dos gafanhotos e seus respectivos quantitativos. */
-SELECT profissao, count(*)
+SELECT profissao, COUNT(*)
 FROM gafanhotos
 GROUP BY profissao
 ORDER BY count(*);
 
 /* 11) Quantos gafanhotos homens e quantas mulheres nasceram após 01/Jan/2005? */
-SELECT sexo, count(*)
+SELECT sexo, COUNT(*)
 FROM gafanhotos
 WHERE nascimento > '2005-01-01'
 GROUP by sexo
@@ -247,7 +247,12 @@ SELECT * FROM gafanhotos WHERE nascimento > '2005-01-01';
 
 /* 12) Uma lista com os gafanhotos que nasceram fora do Brasil, mostrando o país de origem e o total de pessoas nascidas lá,
 Porém nos intessam os países que tiverem mais de 3 gafanhotos com essa nacionalidade. */
-
+SELECT nacionalidade, COUNT(*)
+FROM gafanhotos
+WHERE nacionalidade <> 'Brasil'
+GROUP BY nacionalidade
+HAVING COUNT(nacionalidade) > 3
+ORDER BY COUNT(*);
 
 /* 13) Uma lista agrupada pela altura dos gafanhotos, mostrando quantas pessoas pesam mais de 100Kg e que estão acima da média de altura 
 de todos os cadastrados. */
